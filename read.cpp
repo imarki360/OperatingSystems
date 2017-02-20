7517c8c7238adbc8d78903c236e8ee415e68d241
Mark DeGenova <imarki360@gmail.com>
2017-02-20 16:28:42 -0500

    Initial

---
+++ read.cpp
@@ -0,0 +1,96 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ext2fs/ext2_fs.h>
+#include "read.h"
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <stdlib.h>
+#include <iomanip>
+
+int getRealPage(__s32 image_map[], int Page, int diskSize);
+//template <class T>
+int getData(char data[], int fd, VirtualBox header, __s32 image_map[], int byteStart, int bytes, int arraySize);
+
+int main()
+{
+	struct VirtualBox header;
+	struct BootSector boot;
+	char pathname[280] = "Debian1.0.vdi";
+	int file = open(pathname,O_RDONLY);
+	int length = lseek(file,0,SEEK_END) + 1;
+
+	//printf("%d", length);
+	lseek(file, 0, SEEK_SET);
+	//char* buf = new char[length];
+	read(file, &header, sizeof(header));
+	//printf("%x\n%x\n\n", header.offset_data, 4 * (header.disk_size_bytes >> 20));
+	lseek(file, header.offset_blocks, SEEK_SET);
+	//Read in image map 32-bit entries
+	__s32* image_map = new __s32[header.disk_size_bytes >> 20]; //malloc(sizeof(__s32) * (header.disk_size_bytes >> 20));//
+	read(file,image_map, (header.disk_size_bytes >> 20));
+
+	int arraySize = sizeof(512);
+	char* data = new char[512];//malloc(sizeof(char) * length);
+
+	getData(data, file, header, image_map, 0, 512, 512);
+	printf("%x\n", data[512]);
+	memcpy(&boot, data, arraySize);
+	printf("%x\n", boot.magic);
+
+	return 0;
+}
+
+//template <class T>
+int getData(char data[], int fd, VirtualBox header, __s32 image_map[], int byteStart, int bytes, int arraySize)
+{
+
+	int byteDiff = bytes - byteStart;
+	int virtualPageStart = byteStart >> 20;
+	int currentBytes = 0;
+	int currentPage = virtualPageStart;
+
+	while(byteDiff - currentBytes > 0)
+	{
+		int realPage = getRealPage(image_map, currentPage, header.disk_size_bytes);
+		int pageOffset = header.offset_data + byteStart;
+		int bytesToRead = byteDiff - currentBytes;
+		char* dataBuffer = new char[bytesToRead];//malloc(sizeof(char) * bytesToRead);
+
+		if (realPage << 20 >= 0)
+		{
+			pageOffset += (realPage << 20) + byteStart;
+			lseek(fd,pageOffset,SEEK_SET);
+			read(fd, dataBuffer, bytesToRead);
+		}
+		else if (realPage == -1)
+		{
+			memset(dataBuffer,0, bytesToRead);
+		}
+		for (int j = 0; j < bytesToRead; j++)
+		{
+			data[currentBytes + j] = dataBuffer[j];
+		}
+		currentBytes += bytesToRead;
+		currentPage++;
+	}
+
+	return 0;
+}
+
+
+/*
+* argv __u32[] image_map
+* argv int location = virtual location of page on disk
+*
+* Return: int - actual location of page on disk.
+*/
+int getRealPage(__s32 image_map[], int Page, int diskSize)
+{
+	//add check for out of disksize
+	return image_map[Page];
+}
---
+++ read.h
@@ -0,0 +1,82 @@
+#include "ext2_fs.h"
+//#include <fstream>
+//#include <iostream>
+
+struct __attribute__((packed)) VirtualBox {
+  char description[0x3f];
+  __u32 image_signiture;
+  __u32 version;
+  __u32 header_size;
+  __u32 image_type;
+  char stuff[0x104];
+  __u32 offset_blocks;
+  __u32 offset_data;
+  __u32 cylinders;
+  __u32 heads;
+  __u32 sectors;
+  __u32 sector_size;
+  __u32 garbage;
+  __u64 disk_size_bytes;
+  __u32 block_size;
+  __u32 block_extra_data;
+  __u32 blocks_in_hdd;
+  __u32 blocks_allocated;
+  __u32 uuid_vdi[4];
+  __u32 uuid_link[4];
+  __u32 uuid_parent[4];
+};
+
+struct __attribute__((packed)) Superblock {
+
+};
+
+const __u16 BOOT_SECTOR_MAGIC = 0xaa55;
+
+// an entry in the partition table. the unused entries really hold information,
+// but the information is antiquated and not useful for our purposes here
+struct PartitionEntry {
+  __u8
+    unused0[4],
+    type,
+    unused1[3];
+  __u32
+    firstSector,
+    nSectors;
+};
+
+// the master boot record.
+// the __attribute__((packed)) tells GCC to not add hidden padding between
+// fields. normally, padding is added to improve data access (it's faster if
+// data starts on 4- or 8-byte boundaries). without packing, BootSector is
+// 516 bytes, which is 4 too many.
+//
+// note: you want entry 0 in partitionTable.
+struct __attribute__((packed)) BootSector {
+  __u8
+    unused0[0x1be];
+  PartitionEntry
+    partitionTable[4];
+  __u16
+    magic;
+};
+
+/*
+struct Reader
+{
+    Reader(std::ostream& os) : m_log(os.rdbuf()) { }
+
+    std::streambuf* reset(std::ostream& os)
+    {
+        return m_log.rdbuf(os.rdbuf());
+    }
+
+    template <typename T> friend Reader& operator<<(Reader& os, const T& t)
+    { os.m_log << t; return os; }
+
+    friend Reader& operator<<(Reader& os, std::ostream& ( *pf )(std::ostream&))
+    { os.m_log << pf; return os; }
+
+  private:
+    std::ostream m_log;
+};
+*/
